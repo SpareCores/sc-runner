0537dde9271ba267c1373824b0872295c87685d2
bra-fsn <nagy.attila@gmail.com>
2024-08-20 16:44:13 +0200

    Support user_data/disk_size directly/uniformly

--- src/sc_runner/resources/gcp.py
+++ src/sc_runner/resources/gcp.py
@@ -1,3 +1,5 @@
+import base64
+
 from .. import DefaultOpt, JSON
 from .. import data
 from .base import StackName, default, defaults
@@ -12,7 +14,7 @@ import pulumi_gcp as gcp
 DEFAULTS = {
     "instance_opts": ("GCP_INSTANCE_OPTS", dict(labels={"created-by": "sc-runner"})),
     "bootdisk_opts": ("GCP_BOOTDISK_OPTS", dict()),
-    "bootdisk_init_opts": ("GCP_BOOTDISK_INIT_OPTS", dict(image="ubuntu-2404-lts-amd64", size=16)),
+    "bootdisk_init_opts": ("GCP_BOOTDISK_INIT_OPTS", dict(image="ubuntu-2404-lts-amd64")),
     "scheduling_opts": ("GCP_SCHEDULING_OPTS", dict()),
 }
 
@@ -24,12 +26,19 @@ def resources_gcp(
         bootdisk_opts: Annotated[str, DefaultOpt(["--bootdisk-opts"], type=JSON, default=defaults(DEFAULTS, "bootdisk_opts"), help="Pulumi gcp.compute.InstanceBootDiskArgs options")] = default(DEFAULTS, "bootdisk_opts"),
         bootdisk_init_opts: Annotated[str, DefaultOpt(["--bootdisk-init-opts"], type=JSON, default=defaults(DEFAULTS, "bootdisk_init_opts"), help="Pulumi gcp.compute.InstanceBootDiskInitializeParamsArgs options")] = default(DEFAULTS, "bootdisk_init_opts"),
         scheduling_opts: Annotated[str, DefaultOpt(["--scheduling-opts"], type=JSON, default=defaults(DEFAULTS, "scheduling_opts"), help="Pulumi gcp.compute.InstanceSchedulingArgs options")] = default(DEFAULTS, "scheduling_opts"),
+        user_data: Annotated[str | None, DefaultOpt(["--user-data"], type=str, help="Base64 encoded string with user_data script to run at boot")] = os.environ.get("USER_DATA", None),
+        disk_size: Annotated[int, DefaultOpt(["--disk-size"], type=int, help="Boot disk size in GiBs")] = int(os.environ.get("DISK_SIZE", 30)),
 ):
     if "zone" in instance_opts:
         # as zone is part of the Pulumi stack name, it must be specified in the zone option and not in instance_opts
         raise ValueError("zone must be specified in the zone option")
     # we don't want to modify the default
     instance_opts = copy.deepcopy(instance_opts)
+    if user_data:
+        instance_opts["metadata_startup_script"] = base64.b64decode(user_data)
+    if disk_size:
+        bootdisk_init_opts["size"] = disk_size
+
     provider = gcp.Provider(
         resource_name=zone,
         zone=zone,
