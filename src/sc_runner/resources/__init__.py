b03621cee3d888bde09141797048515d9b91d6cf
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-12-11 19:41:35 +0100

    __all__ instead of explicit reimports with "as"

--- src/sc_runner/resources/__init__.py
+++ src/sc_runner/resources/__init__.py
@@ -1,12 +1,12 @@
 import inspect
 import sys
 
-from .aws import resources_aws as resources_aws
-from .azure import resources_azure as resources_azure
+from .aws import resources_aws
+from .azure import resources_azure
 from .base import *
-from .gcp import resources_gcp as resources_gcp
-from .hcloud import resources_hcloud as resources_hcloud
-from .upcloud import resources_upcloud as resources_upcloud
+from .gcp import resources_gcp
+from .hcloud import resources_hcloud
+from .upcloud import resources_upcloud
 
 # method name prefix for initializing vendor-specific resources
 PREFIX = "resources_"
@@ -17,3 +17,13 @@ supported_vendors = {
     for name, _ in inspect.getmembers(sys.modules[__name__], inspect.isfunction)
     if name.startswith(PREFIX)
 }
+
+
+__all__ = [
+    "resources_aws",
+    "resources_azure",
+    "resources_gcp",
+    "resources_hcloud",
+    "resources_upcloud",
+    "supported_vendors",
+]
